8a9383ae841a8846c94c8acf3ce27f303715a1a1
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-02-07 18:40:38 +0900

    feat:ch1_1

--- src/atomic/ch1/ch1_1/mod.rs
+++ src/atomic/ch1/ch1_1/mod.rs
@@ -5,15 +5,16 @@ pub fn main() {
     thread::spawn(f);
     println!("Hello from the main thread");
 
-    // let t1 = thread::spawn(f);
-    // let t2 = thread::spawn(f);
-    // println!("Hello from the main thread");
+    let t1 = thread::spawn(f);
+    let t2 = thread::spawn(f);
+    println!("Hello from the main thread");
 
-    // t1.join().unwrap();
-    // t2.join().unwrap();
+    t1.join().unwrap();
+    t2.join().unwrap();
 
     let numbers = vec![1, 2, 3];
-
+    //변수 numbers의 소유권이 새로 만들어진 스레드로 이동
+    //move를 사용하지 않았다면 에러발생 소유권을 빌린 numbers보다 스레드가 더 오래 지속할수도 있기떄문
     thread::spawn(move || {
         for n in numbers {
             println!("{n}");
@@ -28,14 +29,14 @@ pub fn main() {
     let t = thread::spawn(move || {
         let len = numbers.len();
         let sum = numbers.into_iter().sum::<usize>();
-        sum / len;
+        sum / len
     });
     let average = t.join().unwrap();
-    // println!("average:{average}");
+    println!("average:{average}");
 }
 
 fn f() {
     println!("Hello from another thread!");
-    let id = thread::current().id();
+    let id = thread::current().id(); //스레드 식별자
     println!("This is my thread id: {id:?}");
 }
