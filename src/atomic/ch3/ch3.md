f2157f09f8b4b07cd4f834112da61d94c2c45510
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-02-19 16:45:10 +0900

    feat:ch3

--- src/atomic/ch3/ch3.md
+++ src/atomic/ch3/ch3.md
@@ -1,3 +1,22 @@
 # 메모리 순서
 
 ## 메모리 순서 재정렬 최적화
+
+
+- 느슨한 순서:Relaxed
+- 순서의 해제와 획득:Release,Acquire,AcqRel
+- SeqCst
+
+
+## happens-before관계
+
+- f(); g();가 있다면 f가 실행된 다음 g가 실행
+- 스레드 사이에서는 특정상황에서만 적용
+- Relaxed에서는 메모리 순서가 아닌 스레드 생성,조인 ,뮤텍스 잠금과 해제,아토믹 연산과 같으 ㄴ경우
+- Relaxed메모리 순서는 가장 기본적이고 가장 성능이 뛰어난 순서
+
+## 해제 순서와 획득 순서
+- happens-before관계를 만들기 위해서는 해제 순서와 획득순서를 사용할수 있다.
+- Release메모리 순서는 저장연산
+- Acquire메모리 순서는 읽기 연산
+- happens-before관계는 acquire-load연산이 release-store연산의 결과를 관찰할떄 만들어진다.
--- src/atomic/ch3/ch3_5.rs
+++ src/atomic/ch3/ch3_5.rs
@@ -1,7 +1,21 @@
-fn f(){
-    
-}
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use std::time::Duration;
+use std::{
+    sync::atomic::{AtomicBool, AtomicU32},
+    thread,
+};
+static DATA: AtomicU32 = AtomicU32::new(0);
+static READY: AtomicBool = AtomicBool::new(false);
 
 pub fn main() {
+    thread::spawn(|| {
+        DATA.store(123, Relaxed);
+        READY.store(true, Release);
+    });
 
+    while !READY.load(Acquire) {
+        thread::sleep(Duration::from_millis(100));
+        println!("waiting");
+    }
+    println!("{}", DATA.load(Relaxed));
 }
