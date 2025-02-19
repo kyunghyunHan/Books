ea3b885386c253be6e5cf6ba98314c057f8bb197
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-02-19 17:46:12 +0900

    feat:test

--- src/atomic/ch3/ch3_5.rs
+++ src/atomic/ch3/ch3_5.rs
@@ -4,18 +4,30 @@ use std::{
     sync::atomic::{AtomicBool, AtomicU32},
     thread,
 };
-static DATA: AtomicU32 = AtomicU32::new(0);
+static mut DATA: u64 = 0;
+
+// static DATA: AtomicU32 = AtomicU32::new(0);
 static READY: AtomicBool = AtomicBool::new(false);
 
 pub fn main() {
     thread::spawn(|| {
-        DATA.store(123, Relaxed);
-        READY.store(true, Release);
+        //안전함:아직 Ready 플래그를 성정하지 않아서
+        // DATA.store(123, Relaxed);
+        unsafe { DATA = 123 }
+        READY.store(true, Release); //이 저장 연산 이전의 모든 값들은
     });
 
     while !READY.load(Acquire) {
+        //여기서 true를 불러온 이후에 확인 가능
         thread::sleep(Duration::from_millis(100));
         println!("waiting");
     }
-    println!("{}", DATA.load(Relaxed));
+    //안전함:READY가 설정되어서 어떤 스레드도 DATA를 변경하지 않음
+    println!("{}", unsafe {
+        {
+            DATA
+        }
+    });
+
+    // println!("{}", DATA.load(Relaxed));
 }
